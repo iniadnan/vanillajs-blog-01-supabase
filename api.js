5d160bdc7082b4bf559373b884ac563961dfd6f9
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-08-28 22:45:47 +0700

    add readme.md and remove api, key supabase

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 // SET API SUPABASE
-const API_URL = "https://vfkbqzbgkvktbyiudlay.supabase.co";
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZma2JxemJna3ZrdGJ5aXVkbGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2Njg1NTYsImV4cCI6MjAwODI0NDU1Nn0.vn8gN0sc8HAVTAnuOUwsATCtPj68mMUiQQ_EMtJfdBk";
+const API_URL = "";
+const API_KEY = "";
 const SUPABASE = supabase.createClient(API_URL, API_KEY);
 
 // SET DOM
---
+++ readme.md
@@ -0,0 +1,2 @@
+# About This Project!
+This project just for learning Javascript and Try to using Supabase for handle Data!
